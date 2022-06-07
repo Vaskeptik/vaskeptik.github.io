8897ac03d1b1ecceca2f0eb520816a2d2d2e3835
Vaskeptik <103656823+Vaskeptik@users.noreply.github.com>
2022-06-07 15:34:44 +0300

    new changes

--- js/main.js
+++ js/main.js
@@ -3,5 +3,28 @@ $(document).ready(function () {
 		if ($(this).find('input').prop('checked') == true) {
 			$(this).addClass('active');
 		}
+	});
+	$(document).on('click', '.checkbox', function (event) {
+		if ($(this).hasClass('active')) {
+			$(this).find('input').prop('checked', false);
+		} else {
+			$(this).find('input').prop('checked', true);
+		}
+		$(this).toggleClass('active');
+		return false;
+	});
+	// radio
+	$.each($('.radiobuttons__item'), function (index, val) {
+		if ($(this).find('input').prop('checked') == true) {
+			$(this).addClass('active');
+		}
 	})
-})
+	$(document).on('click', '.radiobuttons__item', function (event) {
+		$(this).parents('.radiobuttons').find('.radiobuttons__item').removeClass('active');
+		$(this).parents('.radiobuttons').find('.radiobuttons__item input').prop('checked', false);
+		$(this).toggleClass('active');
+		$(this).find('input').prop('checked', true);
+		return false;
+	})
+});
+
